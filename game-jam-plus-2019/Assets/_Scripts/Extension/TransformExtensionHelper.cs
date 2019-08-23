30105a54119b162104dcdacd0757dbb7e9d7d158
Sean Keiffer Lee <seankeiffer@gmail.com>
2019-08-24 04:20:34 +0800

    Radial spawner lightning vfx update

---
+++ game-jam-plus-2019/Assets/_Scripts/Extension/TransformExtensionHelper.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class TransformExtensionHelper
+{
+    public static void SetPosition(this GameObject gameObject, Vector3 position)
+    {
+        gameObject.transform.position = position;
+    }
+}
