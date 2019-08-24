23e774c7f8fbba75bff6b65fa705310b61d3f37f
Sean Keiffer Lee <seankeiffer@gmail.com>
2019-08-24 17:39:47 +0800

    + Added earthquake animation

--- game-jam-plus-2019/Assets/_Scripts/Earthquake/Earthquake.cs
+++ game-jam-plus-2019/Assets/_Scripts/Earthquake/Earthquake.cs
@@ -8,6 +8,9 @@ public class Earthquake : MonoBehaviour
     public VRTK_VelocityEstimator leftEstimator;
     public VRTK_VelocityEstimator rightEstimator;
 
+    public float requiredSpeed;
+
+    private bool canShake;
     private float leftHandSpeed;
     private float rightHandSpeed;
 
@@ -15,5 +18,17 @@ public class Earthquake : MonoBehaviour
     {
         leftHandSpeed = leftEstimator.GetVelocityEstimate().magnitude;
         rightHandSpeed = rightEstimator.GetVelocityEstimate().magnitude;
+
+        if (leftHandSpeed >= requiredSpeed && rightHandSpeed >= requiredSpeed)
+        {
+            canShake = true;
+        }
+    }
+
+    private void OnCollsionEnter(Collision other)
+    {
+        if(!other.collider.CompareTag("PlayerHand")) return;
+        
+        this.GetComponent<Animator>().Play("Earthquake");
     }
 }
