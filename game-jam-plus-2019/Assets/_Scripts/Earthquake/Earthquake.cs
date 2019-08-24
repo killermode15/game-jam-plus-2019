0dd18e2a84263c8b4055e2c92a581d8a6ceb9500
Sean Keiffer Lee <seankeiffer@gmail.com>
2019-08-24 15:59:46 +0800

    Added Earthquke script

---
+++ game-jam-plus-2019/Assets/_Scripts/Earthquake/Earthquake.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class Earthquake : MonoBehaviour
+{
+    public VRTK_VelocityEstimator leftEstimator;
+    public VRTK_VelocityEstimator rightEstimator;
+
+    private float leftHandSpeed;
+    private float rightHandSpeed;
+
+    private void Update()
+    {
+        leftHandSpeed = leftEstimator.GetVelocityEstimate().magnitude;
+        rightHandSpeed = rightEstimator.GetVelocityEstimate().magnitude;
+    }
+}
